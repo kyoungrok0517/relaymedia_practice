ab08ab6fae5013e5aec68a068d1f83224c27caad
Kyoung-Rok Jang <astray0924@gmail.com>
2012-11-04 23:36:54 +0900

    sdfsdf

--- threads/models.py
+++ threads/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Thread(models.Model):
+    owner = models.ForeignKey(User)
+    
+class ThreadItem(models.Model):
+    writer = models.ForeignKey(User)
+    thread = models.ForeignKey(Thread)
+    text = models.TextField(max_length="500")
